7ec6e99e27f1531ce674a43feb859ca8628456d2
Frankie Mercado <oeluer@gmail.com>
2019-07-30 20:47:24 -0700

    Grab elements to reduce getElementById calls

--- script.js
+++ script.js
@@ -1,50 +1,50 @@
 let list = {};
 
-document.getElementById('submitNames').addEventListener('click', addToList);
-document.getElementById('displayNames').addEventListener('click', toggleList);
-document.getElementById('clearNames').addEventListener('click', clearList);
-document.getElementById('generateName').addEventListener('click', generateFake);
-document.getElementById('updateAnon').addEventListener('click', generateAnon);
-// Start list hidden
-document.getElementById('nameList').style.display = 'none';
+const submitNames = document.getElementById('submitNames');
+const displayNames = document.getElementById('displayNames');
+const clearNames = document.getElementById('clearNames');
+const generateName = document.getElementById('generateName');
+const updateAnon = document.getElementById('updateAnon');
+const nameList = document.getElementById('nameList');
+const realName = document.getElementById('realName');
+const secretName = document.getElementById('secretName');
 
-function addToList() {
-  let realName = document.getElementById('realName').value;
-  list[realName] = sanitizeName(document.getElementById('secretName').value);
+const clearForm = () => {
+  realName.value = '';
+  secretName.value = '';
+}
+
+const updateList = () => {
+  nameList.textContent = JSON.stringify(list, null, 2);
+}
+
+const addToList = () => {
+  list[realName.value] = sanitizeName(secretName.value);
   clearForm();
   updateList();
 }
 
-function clearList() {
+const clearList = () => {
   list = {};
   updateList();
 }
 
-function clearForm() {
-  document.getElementById('realName').value = '';
-  document.getElementById('secretName').value = '';
-}
-
-function updateList() {
-  document.getElementById('nameList').textContent = JSON.stringify(list, null, 2);
-}
-
-function sanitizeName(raw) {
+const sanitizeName = raw => {
   //TODO different rules for allowed characters etc.
   return raw.toUpperCase().replace(/[^A-Z]/g, '').trim();
 }
 
-function toggleList() {
-  toggleDisplay('nameList');
-  document.getElementById('displayNames').value = document.getElementById('displayNames').value.includes('Show') ? 'Hide List' : 'Show List';
-}
-
-function toggleDisplay(id) {
+const toggleDisplay = id => {
   let elt = document.getElementById(id);
   elt.style.display = elt.style.display === 'none' ? 'block' : 'none';
 }
 
-function shuffle(a) {
+const toggleList = () => {
+  toggleDisplay('nameList');
+  displayNames.value = displayNames.value.includes('Show') ? 'Hide List' : 'Show List';
+}
+
+const shuffle = a => {
   for (let i = 0; i < a.length - 1; i++) {
     let j = i + Math.floor(Math.random() * (a.length - i));
     [a[i], a[j]] = [a[j], a[i]];
@@ -52,24 +52,35 @@ function shuffle(a) {
   return a;
 }
 
-function generateAnon() {
+const generateAnon = () => {
   let anonList = shuffle(Object.values(list));
-  document.getElementById('anonList').textContent = JSON.stringify(anonList, null, 2);
+  anonList.textContent = JSON.stringify(anonList, null, 2);
 }
 
-async function generateName() {
+const generateWord = async () => {
   const url = 'https://raw.githubusercontent.com/first20hours/google-10000-english/master/google-10000-english-usa-no-swears.txt';
   const response = await fetch(url);
   const text = await response.text();
   return text.split('\n')[Math.floor(Math.random() * 10000)];
 }
 
-async function generateFake() {
-  document.getElementById('realName').value = 'Fake' + Date.now();
-  document.getElementById('secretName').value = await generateName();
+const generateFake = async () => {
+  realName.value = 'Fake' + Date.now();
+  secretName.value = await generateWord();
   addToList();
 }
 
+
+// Add listeners to buttons
+submitNames.addEventListener('click', addToList);
+displayNames.addEventListener('click', toggleList);
+clearNames.addEventListener('click', clearList);
+generateName.addEventListener('click', generateFake);
+updateAnon.addEventListener('click', generateAnon);
+nameList.style.display = 'none'; // Start list hidden
+
+
+// Submit on enter
 document.onkeydown = function (e) {
   e = e || window.event;
   switch (e.which || e.keyCode) {
@@ -78,7 +89,6 @@ document.onkeydown = function (e) {
       break;
   }
 }
-
 
 // let svg = d3.select("body")
 //     .append("svg")
